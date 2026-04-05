add287f89467335787c9e1b27982d0cdd7eb54ce
unknown <ankjjf@gmail.com>
2026-04-05 17:23:58 +0530

    fix: removed tailwind vite plugin

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
